38c35926d55390fd9f3d7b19173fb1694139afeb
Andrey Romanov <andrey.a.romanov@gmail.com>
2024-05-23 20:08:27 +0300

    Fix typo

--- sheet_1.py
+++ sheet_1.py
@@ -85,7 +85,7 @@ class TearOffCalendarSheet(TearOffCalendarBaseSheet):
         #draw[0].rectangle((60, 85, 240, 110), outline = 0)
         month_font = ImageFont.truetype(os.path.join(self.fonts_path, 'PlayfairDisplay-ExtraBold.ttf'), 32)
         month_w, month_h = draw[self.BLACK].textsize(month, font=month_font)
-        draw[j].text(((self.page_w-month_w)/2, y), month, font=month_font)z
+        draw[j].text(((self.page_w-month_w)/2, y), month, font=month_font)
 
         '''
             Draw weekday
