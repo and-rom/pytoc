7d4aed29ff408591916e83d72fa50981fe3b7b0a
Andrey Romanov <andrey.a.romanov@gmail.com>
2025-01-15 22:24:57 +0300

    Tweak _2 template

--- sheet_2.py
+++ sheet_2.py
@@ -17,18 +17,18 @@ class TearOffCalendarSheet(TearOffCalendarBaseSheet):
     def draw(self, cal_data = None):
         super().draw(cal_data)
 
-        self.draw_decor_three_hlines(self.draw[self.j], 202, 10, (3, 2, 1), (0, 1))
+        self.draw_decor_three_hlines(self.draw[self.j], 195, 10, (3, 2, 1), (0, 1))
         self.draw_wifi(self.pages[self.BLACK], self.cal_data['wifi_qlt'], 1, 2)
         self.draw_battery(self.pages[self.BLACK], self.cal_data['battery'], 'r-1', 2)
-        self.draw_day(self.draw[self.i], self.cal_data['day'], 235, 'Molot.otf', 130)
-        self.draw_month(self.draw[self.j], self.cal_data['month'], 10, 205, 'ZenAntiqueSoft-Regular.ttf', 20)
-        self.draw_weekday(self.draw[self.i], self.cal_data['weekday'], 'r-10', 205, 'ZenAntiqueSoft-Regular.ttf', 20)
+        self.draw_day(self.draw[self.i], self.cal_data['day'], 228, 'Molot.otf', 130)
+        self.draw_month(self.draw[self.j], self.cal_data['month'], 10, 198, 'ZenAntiqueSoft-Regular.ttf', 20)
+        self.draw_weekday(self.draw[self.i], self.cal_data['weekday'], 'r-10', 198, 'ZenAntiqueSoft-Regular.ttf', 20)
         if self.cal_data['holiday']:
             logger.debug('Red goes on black for holiday: ' + 'yes' if self.j == 0 else 'no')
             self.draw_holiday_title(self.draw[self.j], self.cal_data['holiday_title'], self.cal_data['holiday_type'], 153, 10, 110, 'Cuprum-Bold.ttf', 32)
-        self.draw_sun_info(self.draw[self.BLACK], self.cal_data['sun_info'], 'Восход\n{}\nЗаход\n{}\nДолгота\nдня\n{}', 9, 236, 'Cuprum-Regular.ttf', 22, 10, 260, 'Cuprum-Regular.ttf', 14)
-        self.draw_moon_info(self.draw[self.BLACK], self.cal_data['moon_info'], 'Заход\n{}\nВосход\n{}\n{}\nд. {}', 272, 239, 'moon_phases.ttf', 18, 'r-10', 260, 'Cuprum-Regular.ttf', 14)
-        self.draw_constellation(self.draw[self.BLACK], self.cal_data['moon_info']['constellation'], 370, 'Cuprum-Italic.ttf', 16)
+        self.draw_sun_info(self.draw[self.BLACK], self.cal_data['sun_info'], 'Восход\n{}\nЗаход\n{}\nДолгота\nдня\n{}', 9, 229, 'Cuprum-Regular.ttf', 22, 10, 253, 'Cuprum-Regular.ttf', 16)
+        self.draw_moon_info(self.draw[self.BLACK], self.cal_data['moon_info'], 'Заход\n{}\nВосход\n{}\n{}\nд. {}', 272, 232, 'moon_phases.ttf', 18, 'r-10', 253, 'Cuprum-Regular.ttf', 16)
+        self.draw_constellation(self.draw[self.BLACK], self.cal_data['moon_info']['constellation'], 363, 'Cuprum-Italic.ttf', 16)
         if self.cal_data['forecast']:
             self.draw_forecast(self.pages[self.BLACK], self.draw[self.BLACK], self.cal_data['forecast'], 25, 10, (25, 43, 25), 'Cuprum-Regular.ttf', 20)
         else:
